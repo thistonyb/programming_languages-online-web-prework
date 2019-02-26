7f696a8a903111f8a3f0c3e83b42928e5e8e2d42
Tony Baker <this.tonybaker@gmail.com>
2019-02-26 22:17:14 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,9 +1,9 @@
 def reformat_languages(languages)
   new_hash = {}
-  languages.each do |language_type, languages|
-    languages[:language_type].each do |info_key, details|
-      new_hash[info_key] = details
-      new_hash[key][:style] = [:language_type]
+  languages.each do |language_type, language_hash|
+    languages[:language_type].each do |language_key, details_hash|
+      new_hash[language_key] = details_hash
+      new_hash[language_key][:style] = [:language_type]
       if key == :javascript
         value[:style] << :functional
       end
