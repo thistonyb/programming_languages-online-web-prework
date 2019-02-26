e2740cb60289be10229f817a7b2514a8279926d8
Tony Baker <this.tonybaker@gmail.com>
2019-02-26 22:27:16 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,10 +1,10 @@
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |language_type, language_hash|
-    languages[:language_type].each do |language_key, details_hash|
+    language_hash.each do |language_key, details_hash|
       if !new_hash.include?(language_key)
         new_hash[language_key] = details_hash
-        new_hash[language_key][:style] = [:language_type]
+        new_hash[language_key][:style] = [language_type]
       elsif
       end
     end
